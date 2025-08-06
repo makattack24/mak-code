7cb194c810eb52209eeb3125b7d4dc92a8232584
ThisAintMilk <taymak@live.com>
2025-08-05 20:52:46 -0600

    Add Angular environment files to .gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-
-export const environment = {
-  production: true,
-  auth0: {
-    domain: 'dev-ylhaasp72045bv63.us.auth0.com',
-    clientId: 'wp1dDm76h9cJvvPeJCRGmw8Pj2GFIxEH',
-    audience: 'https://techwithmak.com'
-  }
-};
--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-
-export const environment = {
-  production: false,
-  auth0: {
-    domain: 'dev-ylhaasp72045bv63.us.auth0.com',
-    clientId: 'wp1dDm76h9cJvvPeJCRGmw8Pj2GFIxEH',
-    audience: 'https://techwithmak.com'
-  }
-};
